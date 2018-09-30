9ad437c8d50bcecc0e1b350346cf938c6713be42
Woodoocubic <guangqingliu1985@gmail.com>
2018-09-30 10:08:31 +1000

    Initial commit

---
+++ src/TodoList.js
@@ -0,0 +1,19 @@
+import React, {Component} from  'react';
+
+class TodoList extends Component {
+    render(){
+        return (
+            <div>
+                <div>
+                    <input />
+                    <button>submit</button>
+                </div>
+                <ul>
+                    <li>dELL</li>
+                </ul>
+            </div>
+        )
+    }
+}
+
+export default TodoList
